9fd24144cccd55c168706e24f65b6119ed4a386f
Dalami <diegoalami@gmail.com>
2023-07-06 21:46:50 -0300

    uso de useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [paises, setPaises] = useState([]);
+  const [busqueda,setBusqueda]= useState('')
+
+  useEffect(() => {
+    const cargarDatos = async () => {
+      try {
+        const response = await fetch('../JSON/paises.json');
+        const data = await response.json();
+        setPaises(data);
+      } catch (error) {
+        console.error('Error al cargar los datos:', error);
+      }
+    };
+
+    cargarDatos();
+  }, []);
+
+   const buscarPais=(e)=>{
+      setBusqueda(e.target.value)
+   }
+
+   const resultado = paises.filter((pais)=>
+    pais.nombre_pais.toLowerCase().includes(busqueda.toLowerCase())
+   )
 
   return (
-    <>
+    <div>
+      <input type="text" placeholder="Buscar país" value={busqueda} onChange={buscarPais}/>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {resultado.map((pais) => (
+          <div key={pais.nombre_pais}>
+            <div>
+              <div>
+                <h2>{pais.nombre_pais}</h2>
+              </div>
+              <ul>
+                <li>Atracciones Principales: {pais.atracciones}</li>
+                <li>Idioma: {pais.idioma}</li>
+                <li>Comidas Populares: {pais.comidas}</li>
+              </ul>
+            </div>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
